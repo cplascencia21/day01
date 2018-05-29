39255842f7a29903a7770085c319559a0bdee731
unknown <cplascencia21@yahoo.com>
2018-05-29 16:51:59 -0700

    day01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  str: string;
+  anotherStr = 'This is another string';
+  yetAnotherStr;
+
+  ngOnInit() {
+    this.str = 'This is a string';
+
+
+    // this.anotherStr = 'This is another string';
+    // this.anotherStr = 4;
+
+    this.yetAnotherStr = 'Another string';
+
+    this.test();
+
+    this.numberArrayInput([]);
+    this.numberArrayInput([5]);
+    this.numberArrayInput([5, 10, 15]);
+    this.numberArrayInput(['string', 5]);
+
+  }
+
+  test() {
+    let name: string = 'Test User';
+    let age: number = 30;
+    let text: string = `My name is ${name} and I am ${age} years old`;
+    console.log(text);
+  }
+
+  numberArrayInput(x: number[]) {
+    console.log(x);
+  }
+
+  anyArrayInput(x: Array<any>) {
+    console.log(x);
+  }
 }
